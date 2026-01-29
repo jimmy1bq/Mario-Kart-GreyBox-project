2d9e733052aa07c36ae616737f3a419fa2c31aa0
jimmy <jimmyzheng8298@gmail.com>
2026-01-29 10:13:13 -0500

    Assest stuff

--- Mario Kart GreyBox Project/Assets/Global Scripts/PlayerController.cs
+++ Mario Kart GreyBox Project/Assets/Global Scripts/PlayerController.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float speed;
+    [SerializeField] float sensitivty;
+    [SerializeField] float horsePower;
+    float forwardSpeed;
+    float turnSpeed;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void Move()
+    {
+        if (Input.GetAxis("Vertical")) 
+        { 
         
+        
+        }
+
     }
 }
